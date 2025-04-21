ebcb3aed7d44d92dd83deb3a973a9113f2b35246
sachinthya <ssrathnavibushana@gmail.com>
2025-04-21 14:42:00 +0530

    initial changes

---
+++ ExpenseTracker/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  ExpenseTracker
+//
+//  Created by sachinthya sunirma rathnavibushana on 2025-04-21.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
---
+++ ExpenseTracker/UICommon/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ExpenseTracker
+//
+//  Created by sachinthya sunirma rathnavibushana on 2025-04-21.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
