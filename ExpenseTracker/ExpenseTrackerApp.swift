fa6ddeed8aebd6d031752bb811a11c0697b21d23
sachinthya <ssrathnavibushana@gmail.com>
2025-04-21 13:31:39 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by sachinthya sunirma rathnavibushana on 2025-04-21.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by sachinthya sunirma rathnavibushana on 2025-04-21.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
