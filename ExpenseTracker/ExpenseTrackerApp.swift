b40226345203cfc5cc58261a59b0286eb8123e63
sachinthya <ssrathnavibushana@gmail.com>
2025-05-03 19:10:42 +0530

    home view

--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -24,7 +24,7 @@ struct ExpenseTrackerApp: App {
                 }
             }else{
                 withAnimation{
-                    AccountListView()
+                    HomeView()
                 }
             }
         }
